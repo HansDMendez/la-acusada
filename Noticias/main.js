60cbd15a63eb7ca230c0ce27b1fe994706fd8c40
HansDMendez <105138715+HansDMendez@users.noreply.github.com>
2022-05-09 16:21:59 -0500

    Add files via upload

---
+++ Noticias/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.Dibujo', { delay: 500 });
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-baneer-two', { delay: 500 });
